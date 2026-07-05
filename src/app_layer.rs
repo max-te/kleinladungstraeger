a8a5741783d039ada55f60d071c427c2625e0d68
Max Teegen <m_teegen@gmx.de>
2026-07-05 16:48:38 +0200

    fix: non-sparse tars
    should fix unhandled tar header type 83 on docker pull

--- src/app_layer.rs
+++ src/app_layer.rs
@@ -9,6 +9,7 @@ fn tar_folder(src_path: impl AsRef<std::path::Path>) -> Result<Vec<u8>> {
     let buf = Vec::new();
     let mut tar = tar::Builder::new(buf);
     tar.follow_symlinks(false);
+    tar.sparse(false);
     tar.mode(tar::HeaderMode::Deterministic);
     tar.append_dir_all("", src_path).into_diagnostic()?;
     tar.into_inner().into_diagnostic()
